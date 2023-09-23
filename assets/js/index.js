49dccbd7c5e9d5506d8aa35a30a8868b5980f89a
Heuristiac <119772726+Heuristiac@users.noreply.github.com>
2023-09-23 12:39:58 -0500

    ok

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,9 @@
 console.log("Entro index.js");
+
+const btnBlue = document.getElementById("btnBlue");
+
+//btnBlue.addEventListener("click", function(){
+  //  console.log("Click en el botosn azul!!!")
+
+
+
